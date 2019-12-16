f5db087411d36eb03330027f1aa6af1afe398ec2
Julia Pickett-Garcia <s199064@students.aie.edu>
2019-12-16 12:09:32 -0800

    Added Cat's AudioManager as an actual script. Set up folder organization. (The cs files in the sprites and sounds folder are just so git recognizes the folders to push. Once we get sounds and sprintes in we can get rid of them.)

--- AIEWinterJam2019/Assets/Scripts/AudioMaganger.cs
+++ AIEWinterJam2019/Assets/Scripts/AudioMaganger.cs
@@ -1,10 +1,28 @@
-using UnityEngine.Audio;
+﻿using UnityEngine.Audio;
 using System;
 using UnityEngine;
+
+[System.Serializable]
+public class Sound
+{
+
+    public string name;
+    public AudioClip clip;
+
+    [Range(0f, 1f)]
+    public float volume;
+    [Range(.1f, 3f)]
+    public float pitch;
+    public bool loop;
+
+    [HideInInspector]
+    public AudioSource source;
+}
 
 public class AudioManager : MonoBehaviour
 {
     public Sound[] sounds;
+
     void Awake()
     {
         foreach (Sound s in sounds)
@@ -12,18 +30,19 @@ public class AudioManager : MonoBehaviour
             s.source = gameObject.AddComponent<AudioSource>();
             s.source.clip = s.clip;
 
-
             s.source.volume = s.volume;
             s.source.pitch = s.pitch;
             s.source.loop = s.loop;
         }
 
     }
-        void Start()
+
+    void Start()
     {
         //Play("Music");
     }
-    public void Play (string name)
+
+    public void Play(string name)
     {
         Sound s = Array.Find(sounds, sound => sound.name == name);
         s.source.Play();
