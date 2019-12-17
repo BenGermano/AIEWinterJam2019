d850f7878976dca315a81c658bf5d17cf64f5c8a
Julia Pickett-Garcia <s199064@students.aie.edu>
2019-12-17 12:04:34 -0800

    Teen AI Beginnings
    WIP of the teen AI- the swing animation enables the attack hitbox, which damages the player. There is now a "Teen" prefab, but it's animator state machine is incomplete.

---
+++ AIEWinterJam2019/Assets/Scripts/TeenEnemyAttackAI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeenEnemyAttackAI : MonoBehaviour
+{
+    public BoxCollider2D attackHitbox;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("oof");
+        }
+    }
+}
