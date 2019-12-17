e38a29ef72aeba40f738335ba73569feed089f4f
BenGermano <58953931+BenGermano@users.noreply.github.com>
2019-12-17 14:57:39 -0800

    Ben - prefab making

---
+++ AIEWinterJam2019/Assets/Scripts/AnimScripts/TeenDownAnim.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeenDownAnim : MonoBehaviour
+{
+    public BaseEnemyAI enemy;
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (enemy.hasPumpkin == false)
+        {
+            animator.SetBool("Down", true);
+            animator.SetBool("CarryUp", false);
+            animator.SetBool("Carving", false);
+        }
+        if (enemy.hasPumpkin == true)
+        {
+            animator.SetBool("CarryUp", true);
+            animator.SetBool("Down", false);
+            animator.SetBool("Carving", false);
+        }
+        if (enemy.isCarving == true)
+        {
+            animator.SetBool("Carving", true);
+            animator.SetBool("CarryDown", false);
+            animator.SetBool("Down", false);
+        }
+    }
+}
---
+++ AIEWinterJam2019/Assets/Scripts/AnimScripts/TeenUpAnim.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeenUpAnim : MonoBehaviour
+{
+    public BaseEnemyAI enemy;
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (enemy.hasPumpkin == false)
+        {
+            animator.SetBool("Up", true);
+            animator.SetBool("CarryDown", false);
+            animator.SetBool("Carving", false);
+        }
+        if(enemy.hasPumpkin == true)
+        {
+            animator.SetBool("CarryDown", true);
+            animator.SetBool("Up", false);
+            animator.SetBool("Carving", false);
+        }
+        if(enemy.isCarving == true)
+        {
+            animator.SetBool("Carving", true);
+            animator.SetBool("CarryDown", false);
+            animator.SetBool("Up", false);
+        }
+    }
+}
