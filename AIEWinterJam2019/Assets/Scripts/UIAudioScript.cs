ed753f98786b1381fff48b8c8eb519149e7b8256
Yerok1292 <50648323+Yerok1292@users.noreply.github.com>
2019-12-17 17:55:51 -0800

    Cat 5:55 12/17
    Added Menu Button SFX on click.

---
+++ AIEWinterJam2019/Assets/Scripts/UIAudioScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIAudioScript : MonoBehaviour
+{
+    public AudioClip sound;
+    private Button button { get { return GetComponent<Button>(); } }
+    private AudioSource source { get { return GetComponent<AudioSource>(); } }
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.AddComponent<AudioSource>();
+        source.clip = sound;
+        source.playOnAwake = false;
+        button.onClick.AddListener(() => PlaySound());
+
+    }
+
+    // Update is called once per frame
+    void PlaySound()
+    {
+        source.PlayOneShot(sound);
+    }
+}
